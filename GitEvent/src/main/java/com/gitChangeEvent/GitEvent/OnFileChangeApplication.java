909582c93a021425945fb2b2dd531123a5814bc9
Vijay-Khairnar <vijaykhairnar987@gmail.com>
2024-10-06 10:30:30 +0530

    Initial commit

---
+++ GitEvent/src/main/java/com/gitChangeEvent/GitEvent/OnFileChangeApplication.java
@@ -0,0 +1,13 @@
+package com.gitChangeEvent.GitEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnFileChangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnFileChangeApplication.class, args);
+	}
+
+}
---
+++ GitEvent/src/test/java/com/gitChangeEvent/GitEvent/OnFileChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitChangeEvent.GitEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnFileChangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
